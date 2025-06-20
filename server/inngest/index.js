d5699d09e116b00c48e8c7e5f120b6afbafcd474
bishal04-tech <bishalmahanta68@gmail.com>
2025-06-20 13:53:28 +0530

    commit 2 spelling issue

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -9,16 +9,20 @@ export const inngest = new Inngest({ id: "movie-ticket-booking" });
   { id: "sync-user-from-clerk" },
   { event: "clerk/user.created" },
   async ({ event }) => {
+
+   
     const {id,first_name,last_name,email_addresses,image_url}=event.data
 
     const userData={
         _id:id,
-        email:email_addresses[0].email_addresses,
+        email:email_addresses[0].email_address,
         name:first_name+' '+last_name,
         image:image_url
     }
     await User.create(userData)
-    
+      
+
+     
   },
 );
 
@@ -46,7 +50,7 @@ const syncUserDeletion = inngest.createFunction(
 
        const userData={
         _id:id,
-        email:email_addresses[0].email_addresses,
+        email:email_addresses[0].email_address,
         name:first_name+' '+last_name,
         image:image_url
     }
